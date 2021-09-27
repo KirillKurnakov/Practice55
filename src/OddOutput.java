30a89e93a6ed32771339f27370a591c03e1b2871
Kirill <90597372+KirillKurnakov@users.noreply.github.com>
2021-09-27 10:06:49 +0300

    Update OddOutput.java

--- src/OddOutput.java
+++ src/OddOutput.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 // 12 задание (вывод нечетных чисел )
 public class OddOutput extends Main {
-    public int Odds() {
+    public void Odds() {
         Scanner in = new Scanner(System.in);
         int hg = in.nextInt();
         if (hg == 0)
             System.exit(0);
         if (hg % 2 == 1) {
             System.out.println(hg);
+            Odds();
         }
-        return Odds();
+        else
+            Odds();
     }
 }
